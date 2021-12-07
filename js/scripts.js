13ceccc95fd64aeb5a249512d39e1410d00127e9
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-12-06 20:30:46 -0800

    Fourth commit working on JS X Y coordinates
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>
    Co-authored-by: Zoe Weinstein <zweinstein@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,24 @@ Board.prototype.addSpace = function(space){
   this.spaces.push(space);
 }
 
+Board.prototype.findSpace = function(xcoord, ycoord){
+  let spaceArray = this.spaces;
+  for(let i=0; i < spaces.length; i++){
+    if(spaces[i].xcoord === xcoord && spaces[i].ycoord === ycoord){
+      return spaces[i];
+    }
+  }
+}
+
+function Space(xcoord, ycoord, marked){
+  this.xcoord = xcoord;
+  this.ycoord = ycoord;
+  this.marked = marked;
+}
+
+
+//ui
+
+
+
+
