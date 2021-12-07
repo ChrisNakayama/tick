7487bd21ddd2b826c6c1b4bb3a0f02e42ac8fa5b
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-12-06 20:36:42 -0800

    Adding TIC TAC TOE JS
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>
    Co-authored-by: Zoe Weinstein <zweinstein@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -21,10 +21,30 @@ Board.prototype.findSpace = function(xcoord, ycoord){
   }
 }
 
+
+
+
 function Space(xcoord, ycoord, marked){
   this.xcoord = xcoord;
   this.ycoord = ycoord;
   this.marked = marked;
+}
+
+function findThreeInARow(space1, space2, space3){
+  if(space1.marked === space2.marked && space1.marked === space3.marked){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+function checkAll(board){
+  let spaceArray = board.spaces;
+  /*[1] [2] [3]
+    [4] [5] [6]
+    [7] [8] [9]
+  */
+  findThreeInARow()
 }
 
 
